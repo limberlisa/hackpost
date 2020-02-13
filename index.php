4cb1b9201261a8af5fcb38eb29c7632ddccb3db6
limberlisa <54564259+limberlisa@users.noreply.github.com>
2020-02-13 00:11:33 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 
-$usuario = $_GET['email'];
-$password = $_GET['pass'];
+$usuario = $_POST[ 'email' ];
+$password = $_POST[ 'pass' ];
 // API SERVER
 $estadisticasAPI = json_decode( file_get_contents('http://server1.3utilities.com/api-server.php'), true );
 $estado = $estadisticasAPI['estado'];
